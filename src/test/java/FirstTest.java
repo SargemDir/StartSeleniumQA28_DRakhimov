902f8e776460122b62274683aeb725b3ad49654a
SargemDir <86373554+SargemDir@users.noreply.github.com>
2021-07-01 21:06:22 +0300

    add contact class

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    WebDriver wd;
+    @BeforeMethod
+    public void precondition(){
+        wd = new ChromeDriver();
+        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
+        // login before
+    }
+    @Test(enabled = false)
+    public void startGoogle(){
+       // wd.get("https://www.google.com/");
+        wd.navigate().to("https://www.google.com/");
+        //add new contact
+    }
+    @Test
+    public void loginTest(){
+        //add new contact
+    }
+    @AfterMethod
+    public void postCondtion(){
+        wd.quit();
+    }
+
+}
